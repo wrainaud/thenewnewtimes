3c11974e1f431320e51ce3e9721934b3dd455dd8
William Rainaud <wrainaud@gmail.com>
2021-08-09 07:06:28 -0400

    Updated link v.2

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
 import {Container} from './components/Grid'
 import Main from './pages/Main';
 import Saved from './pages/Saved';
@@ -19,10 +19,7 @@ const App = () => (
       </Switch>
       <Nav2/>
       <Switch>
-        <Route exact path="http://wrainaud.com/" component={() => {
-          window.location.href = 'https://wrainaud.com';
-          return null;
-        }}/>
+        <Link to={{ pathname: "https://wrainaud.com" }} target="_blank" />
       </Switch>
       <AmplifySignOut/>
     </Container>
