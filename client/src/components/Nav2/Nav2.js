63d3730a1f0f0885340409a9cf55bb7ebbdec032
William Rainaud <wrainaud@gmail.com>
2021-08-09 15:54:07 -0400

    Updated bottom Nav

--- client/src/components/Nav2/Nav2.js
+++ client/src/components/Nav2/Nav2.js
@@ -6,17 +6,7 @@ const Nav2 = () => (
         <Link className="navbar-brand" to="/">
             The NewNew Times
         </Link>
-        <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"/>
-        </button>
-        <div className="collapse navbar-collapse">
+        <div>
             <ul className="navbar-nav">
                 <li className="nav-item">
                     <a className="nav-link" href="https://wrainaud.com/">
